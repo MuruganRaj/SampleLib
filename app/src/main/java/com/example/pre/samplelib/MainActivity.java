e411cad246efdfc203aa5b7c78a338d513eef6b0
Murugan <Murugan@Nucleus.com>
2018-05-17 17:44:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pre/samplelib/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.pre.samplelib;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        takeScreenshot();
+
+//        Socket socket = null;
+//        try {
+//            socket = new Socket("192.168.1.12", 8890);
+//            ParcelFileDescriptor pfd = ParcelFileDescriptor.fromSocket(socket);
+//            MediaRecorder.setOutputFile(pfd.getFileDescriptor());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+
+
+    private void takeScreenshot() {
+        Date now;
+        now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or DOM
+            e.printStackTrace();
+        }
+    }
+
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+}
---
+++ sceencast/src/main/java/com/example/pre/sceencast/StudentMarks.java
@@ -0,0 +1,8 @@
+package com.example.pre.sceencast;
+
+public class StudentMarks {
+
+    public int addMarks(int a , int b){
+        return a+b;
+    }
+}
